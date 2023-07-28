873a1eb04cdda39abc9b63cb775d6641a7bcfadf
cituk <ecituk@thefives.com.mx>
2023-07-27 23:37:06 -0500

    Datasource - Obtener películas en cines

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,10 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body:  Center(
-        child: Text( Enviroment.movieDbKey ),
-      ),
-    );
+    return   Placeholder();
   }
 }
