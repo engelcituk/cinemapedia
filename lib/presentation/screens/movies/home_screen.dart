1b3c2aae79289b44f4dd9071900aa03b3ef79418
cituk <ecituk@thefives.com.mx>
2023-08-11 00:34:13 -0500

    Custom AppBar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -37,17 +38,24 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if (nowPlayingMovies.isEmpty )  return const  CircularProgressIndicator();
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return Material(
-          child: ListTile(
-            title: Text(movie.title),
-            subtitle: Text(movie.originalTitle),
+    return Column(
+      children: [
+        CustomAppbar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return Material(
+                child: ListTile(
+                  title: Text(movie.title),
+                  subtitle: Text(movie.originalTitle),
+                ),
+              );
+            },
           ),
-        );
-      },
+        )
+      ],
     );
   }
 }
